384de24f7331cfda2ba8c250ebd0fb04b545ae4c
tjk113 <tjkuhl1@live.com>
2024-01-08 19:10:46 -0500

    Replaced header title

--- src/html_strings.nim
+++ src/html_strings.nim
@@ -2,12 +2,12 @@ const HEADER* = """<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <title>tjk</title>
+    <title>tjk's food for thought</title>
     <link href="styles.css" type="text/css" rel="stylesheet">
     <script src="index.js" type="text/javascript"></script>
 </head>
 <body>
-    <h1>tjk's minimalistic dev blog</h1>
+    <h1>tjk's food for thought</h1>
     <div class="navBar">
         <a href="index.html">Home</a>
         <a href="https://github.com/tjk113">Github</a>
